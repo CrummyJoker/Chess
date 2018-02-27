8e5ec430ea701483a7fae927ac2b0fecdcfc62f8
emixvier <36886342+emixvier@users.noreply.github.com>
2018-02-27 14:47:53 +0200

    Update Kuningas.java

--- Kuningas.java
+++ Kuningas.java
@@ -1,8 +1,8 @@
 public class Kuningas extends Nappula{
-  private final char kuningas=K;
   private boolean onkoShakki;
   
   Kuningas(Vari vari) {
     super(vari);
+    super.symboli("K");
   }
 }
