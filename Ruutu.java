65f6a61b918433dcb9db94b8f8a8b6db34be8f98
emixvier <36886342+emixvier@users.noreply.github.com>
2018-02-27 14:39:37 +0200

    Update Ruutu.java

--- Ruutu.java
+++ Ruutu.java
@@ -1,3 +1,29 @@
 public class Ruutu{
-
+  Nappula n;
+  String printti;
+  int x;
+  int y;
+  
+  Ruutu(int x,int y) {
+    this.x = x;
+    this.y = y;
+    printti = "---." + " ";
+  }
+  Ruutu(int x,int y,Nappula n) {
+    this.x = x;
+    this.y = y;
+    this.n = n;
+    if (n.vari==Vari.M) {
+      printti =   n.vari + n.symboli  + " ";
+    } else if (n.vari==Vari.V) {
+      printti = n.vari + n.symboli + "  ";
+    }
+  }
+  public String toString() {
+    return printti;
+  }
+  void siirra(int x,int y) {
+    this.x = x;
+    this.y = y;
+  } 
 }
