c58eb7a64b7aca71261e9764124fe943b99a2c3a
emixvier <36886342+emixvier@users.noreply.github.com>
2018-02-27 14:40:31 +0200

    Update Sotlias.java

--- Sotlias.java
+++ Sotlias.java
@@ -1,5 +1,11 @@
 public class Sotilas extends Nappula{
-private final char moukka=S;
+  private final char moukka=S;
   boolean promootio;
   boolean ohesta;
+  
+  Sotilas(Vari vari) {
+    super(vari);
+    super.symboli = "S";
+  }
+  
 }
