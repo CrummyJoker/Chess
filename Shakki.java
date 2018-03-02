f5455527eb66a7a4b1a85c2f5c07aa6843611594
CrummyJoker <36886247+CrummyJoker@users.noreply.github.com>
2018-03-02 12:21:37 +0200

    Update Shakki.java

--- Shakki.java
+++ Shakki.java
@@ -1,18 +1,7 @@
 import java.util.*;
 public class Shakki{
   public static void main(String[] args){
-    Nappula[][] Lauta = new Ruutu[8][8];
-    Nappula[] sotilaat = new Sotilas[16];
-    
-    for (int i=0;i<16;i++) {
-      if (i<8) {
-        Nappula s = new Sotilas(Vari.M,Tyyppi.SOTILAS);
-        sotilaat[i] = s;
-      } else {
-        Nappula s = new Sotilas(Vari.V,Tyyppi.SOTILAS);
-        sotilaat[i] = s;
-      }
-    }
+
     
     //Kuningattaret
     Nappula q1 = new Nappula(Vari.V,Tyyppi.Q,7,3);
@@ -91,87 +80,7 @@ public class Shakki{
     
 
     //alustetaan lauta
-    int sotilasCount = 0;
-    for (int i =0;i<8;i++) {
-      for (int j=0;j<8;j++) {
-        //Tornit
-        if(i==0&&j==0){
-          Ruutu r = new Ruutu(i,j,t3);
-          lauta[i][j] = r;
-          continue;
-        }
-        if(i==0&&j==7){
-          Ruutu r = new Ruutu(i,j,t4);
-          lauta[i][j] = r;
-          continue;
-        }
-        if(i==7&&j==0){
-          Ruutu r = new Ruutu(i,j,t1);
-          lauta[i][j] = r;
-          continue;
-        }
-        if(i==7&&j==7){
-          Ruutu r = new Ruutu(i,j,t2);
-          lauta[i][j] = r;
-          continue;
-        }
-        //Ratsut
-        if(i==0&&j==1){
-           Ruutu r = new Ruutu(i,j,r3);
-          lauta[i][j] = r;
-          continue;
-        }
-        if(i==0 && j==6){
-          Ruutu r = new Ruutu(i,j,r4);
-          lauta[i][j] = r;
-          continue;
-        }
-        if(i==7&&j==1){
-        Ruutu r = new Ruutu(i,j,r1);
-          lauta[i][j] = r;
-          continue;
-        }
-        if(i==7&&j==6){
-          Ruutu r = new Ruutu(i,j,r2);
-          lauta[i][j] = r;
-          continue;
-        }
-        //Lähetit
-        if(i==0&&j==
-        //kuningattaret
-        if(i==7&&j==3) {
-          Ruutu r = new Ruutu(i,j,q2);
-          lauta[i][j] = r;
-          continue;
-        }
-        if(i==0&&j==3) {
-          Ruutu r = new Ruutu(i,j,q1);
-          lauta[i][j] = r;
-          continue;
-        }
-        //sotilaat
-        if((i==1)||(i==6)) {
-          Ruutu r = new Ruutu(i,j,sotilaat[sotilasCount]);
-          sotilasCount++;
-          lauta[i][j] = r;
-          continue;
-        }
-        Ruutu r = new Ruutu(i,j);
-        lauta[i][j] = r;
-      }
-    }
-    System.out.println(lauta[0][3].toString());
-    print(lauta);
-  }
-  
-  static void print(Ruutu[][] lauta) {
-    for (int i=0;i<8;i++) {
-      for(int j=0;j<8;j++) {
-        System.out.print(lauta[i][j].toString());
-      }
-      System.out.println();
-    }
-  System.out.println(" ");
+
   }
 }
 
