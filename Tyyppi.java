47b8385015cb4d92cffbd179da8d996a9c438819
MarttaY <36828471+MarttaY@users.noreply.github.com>
2018-03-02 12:25:00 +0200

    Update Tyyppi.java

--- Tyyppi.java
+++ Tyyppi.java
@@ -12,6 +12,42 @@ public enum Tyyppi{
   }, S("S") {
     boolean promootio;
     boolean ohesta;
+    
+    public boolean onkoSiirtoLaillinen(int Xmista, int Ymista, int Xmihin, int Ymihin) {
+    if (v == Vari.M) {
+      if (Xmihin - Xmista == 1 && (Ymihin - Ymista == -1 || Ymihin - Ymista == 1)) { // syönti: diagonaalinen siirto
+        siirra(); // määrää onSiirretty-attribuutin arvoksi true joka siirrolla. Haittaako?
+        return true;
+      }
+      if (siirretty == false) { // ensimmäinen siirto
+        if ((Xmihin - Xmista == 2 || Xmihin - Xmista == 1) && Ymihin - Ymista == 0) {
+          siirra();
+          return true; //vaihda sotilaan onSiirretty-attribuutin arvoksi true ja palauta true
+        }
+      } else { // ei ensimmäinen siirto
+        if (Xmihin - Xmista == 1 && Ymihin - Ymista == 0) {
+          return true;
+        }
+      }
+    } else { // sama kuin edellä valkeille nappuloille
+      if (Xmihin - Xmista == -1 && (Ymihin - Ymista == -1 || Ymihin - Ymista == 1))
+      { // syönti
+        siirra();  
+        return true;
+      }
+      if (siirretty == false) {
+        if ((Xmihin - Xmista == -2 || Xmihin - Xmista == -1) && Ymihin - Ymista == 0) {
+          siirra();
+          return true;
+        }
+      } else {
+        if (Xmihin - Xmista == -1 && Ymihin - Ymista == 0) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
   
   }, R("R") {
     
