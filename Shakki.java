e0b78abf3a048c9de22376178434de676ed058f6
CrummyJoker <36886247+CrummyJoker@users.noreply.github.com>
2018-03-02 11:57:16 +0200

    Update Shakki.java

--- Shakki.java
+++ Shakki.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class Shakki{
   public static void main(String[] args){
-    Nappula[][] lauta = new Ruutu[8][8];
+    Nappula[][] Lauta = new Ruutu[8][8];
     Nappula[] sotilaat = new Sotilas[16];
     
     for (int i=0;i<16;i++) {
@@ -14,34 +14,82 @@ public class Shakki{
       }
     }
     
-    Nappula q1 = new Nappula(Vari.V);
-    Nappula q2 = new Nappula(Vari.M);
-    
-    Nappula k1 = new Kuningas(Vari.V);
-    Nappula k2= new Kuningas(Vari.M);
-    
-    //torni
+    //Kuningattaret
+    Nappula q1 = new Nappula(Vari.V,Tyyppi.Q);
+    Nappula q2 = new Nappula(Vari.M),Tyyppi.Q;
+    //Kuninkaat
+    Nappula k1 = new Nappula(Vari.V,Tyyppi.K);
+    Nappula k2= new Nappula(Vari.M,Tyyppi.K);
+    //Tornit
     Nappula t1  = new Nappula(Vari.V,Tyyppi.T);
-    Nappula t2  = new Nappula(Vari.V,Tyyppi.T);
-    Nappula t3  = new Nappula(Vari.M,Tyyppi.T);
-    Nappula t4  = new Nappula(Vari.M,Tyyppi.T);
-    lauta[0][0] = t4;
-    lauta[0][7] = t3;
-    lauta[7][0] = t2;
-    lauta[7][7] = t1;
+    Nappula t2  = new Nappula(Vari.V, Tyyppi.T);
+    Nappula t3  = new Nappula(Vari.M, Tyyppi.T);
+    Nappula t4  = new Nappula(Vari.M, Tyyppi.T);
+    //Lähetit
+    Nappula l1 = new Nappula(Vari.V, Tyyppi.L);
+    Nappula l2 = new Nappula(Vari.V, Tyyppi.L);
+    Nappula l3 = new Nappula(Vari.M, Tyyppi.L);
+    Nappula l4 = new Nappula(Vari.M, Tyyppi.L);
+    //Ratsut
+    Nappula r1  =new Nappula(Vari.V,Tyyppi.R);
+    Nappula r2  =new Nappula(Vari.V,Tyyppi.R);
+    Nappula r3  =new Nappula(Vari.M, Tyyppi.R);
+    Nappula r4  =new Nappula(Vari.M,Tyyppi R);
+    //Sotilaat
+    Nappula s1 new Nappula(Vari.V,Tyyppi.S);
+    Nappula s2 new Nappula(Vari.V,Tyyppi.S);
+    Nappula s3 new Nappula(Vari.V,Tyyppi.S);
+    Nappula s4 new Nappula(Vari.V,Tyyppi.S);
+    Nappula s5 new Nappula(Vari.V,Tyyppi.S);
+    Nappula s6 new Nappula(Vari.V,Tyyppi.S);
+    Nappula s7 new Nappula(Vari.V,Tyyppi.S);
+    Nappula s8 new Nappula(Vari.V,Tyyppi.S);
+    Nappula s9 new Nappula(Vari.M,Tyyppi.S);
+    Nappula s10 new Nappula(Vari.M,Tyyppi.S);
+    Nappula s11 new Nappula(Vari.M,Tyyppi.S);
+    Nappula s12 new Nappula(Vari.M,Tyyppi.S);
+    Nappula s13 new Nappula(Vari.M,Tyyppi.S);
+    Nappula s14 new Nappula(Vari.M,Tyyppi.S);
+    Nappula s15 new Nappula(Vari.M,Tyyppi.S);
+    Nappula s16 new Nappula(Vari.M,Tyyppi.S);
+    //Tyhjät
+    Nappula e1 new Nappula(Vari.E,Tyyppi.E);
+    Nappula e2 new Nappula(Vari.E,Tyyppi.E);
+    Nappula e3 new Nappula(Vari.E,Tyyppi.E);
+    Nappula e4 new Nappula(Vari.E,Tyyppi.E);
+    Nappula e5 new Nappula(Vari.E,Tyyppi.E);
+    Nappula e6 new Nappula(Vari.E,Tyyppi.E);
+    Nappula e7 new Nappula(Vari.E,Tyyppi.E);
+    Nappula e8 new Nappula(Vari.E,Tyyppi.E);
+    Nappula e9 new Nappula(Vari.E,Tyyppi.E);
+    Nappula e10 new Nappula(Vari.E,Tyyppi.E);
+    Nappula e11 new Nappula(Vari.E,Tyyppi.E);
+    Nappula e12 new Nappula(Vari.E,Tyyppi.E);
+    Nappula e13 new Nappula(Vari.E,Tyyppi.E);
+    Nappula e14 new Nappula(Vari.E,Tyyppi.E);
+    Nappula e15 new Nappula(Vari.E,Tyyppi.E);
+    Nappula e16 new Nappula(Vari.E,Tyyppi.E);
+    Nappula e17 new Nappula(Vari.E,Tyyppi.E);
+    Nappula e18 new Nappula(Vari.E,Tyyppi.E);
+    Nappula e19 new Nappula(Vari.E,Tyyppi.E);
+    Nappula e20 new Nappula(Vari.E,Tyyppi.E);
+    Nappula e21 new Nappula(Vari.E,Tyyppi.E);
+    Nappula e22 new Nappula(Vari.E,Tyyppi.E);
+    Nappula e23 new Nappula(Vari.E,Tyyppi.E);
+    Nappula e24 new Nappula(Vari.E,Tyyppi.E);
+    Nappula e25 new Nappula(Vari.E,Tyyppi.E);
+    Nappula e26 new Nappula(Vari.E,Tyyppi.E);
+    Nappula e27 new Nappula(Vari.E,Tyyppi.E);
+    Nappula e28 new Nappula(Vari.E,Tyyppi.E);
+    Nappula e29 new Nappula(Vari.E,Tyyppi.E);
+    Nappula e30 new Nappula(Vari.E,Tyyppi.E);
+    Nappula e31 new Nappula(Vari.E,Tyyppi.E);
+    Nappula e32 new Nappula(Vari.E,Tyyppi.E);
+
+
     
-    Nappula l1 = new Lahetti(Vari.V);
-    Nappula l2 = new Lahetti(Vari.V);
-    Nappula l3 = new Lahetti(Vari.M);
-    Nappula l4 = new Lahetti(Vari.M);
     
-    Nappula r1  =new Ratsu(Vari.V);
-    Nappula r2  =new Ratsu(Vari.V);
-    Nappula r3  =new Ratsu(Vari.M);
-    Nappula r4  =new Ratsu(Vari.M);
-    
-    
-    //luodaan muutkin nappulat    
+
     //alustetaan lauta
     int sotilasCount = 0;
     for (int i =0;i<8;i++) {
@@ -49,7 +97,7 @@ public class Shakki{
         //Tornit
         if(i==0&&j==0){
           Ruutu r = new Ruutu(i,j,t3);
-          l[i][j] = r;
+          lauta[i][j] = r;
           continue;
         }
         if(i==0&&j==7){
